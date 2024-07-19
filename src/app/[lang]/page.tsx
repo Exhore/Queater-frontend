170650ca547acf474b4c99a111d961c9c567d228
EXHOR\nelso <nelsonrivca@gmail.com>
2024-07-19 18:17:00 +0100

    setup

--- src/app/[lang]/page.tsx
+++ src/app/[lang]/page.tsx
@@ -1,6 +1,7 @@
 import { GeneralProps } from '@/interfaces/lang-props'
 import { Metadata, ResolvingMetadata } from 'next'
 import { getDictionary } from './dictionaries'
+import { IArrowLarge } from '@/components/atoms/svg/IArrow'
 
 export async function generateMetadata(
   { params: { lang } }: GeneralProps,
@@ -20,5 +21,10 @@ export async function generateMetadata(
 
 export default async function Home({ params: { lang } }: GeneralProps) {
   const dict = await getDictionary(lang)
-  return <main className="flex h-full w-full max-w-8xl flex-col"></main>
+  return (
+    <main className="flex h-full w-full max-w-8xl flex-col">
+      {dict.metadata.title}
+      <IArrowLarge />
+    </main>
+  )
 }
